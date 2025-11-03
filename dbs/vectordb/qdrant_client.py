0fa2e3a98a173677f813acad2c0f777755062702
yasinsb <sbyasin@gmail.com>
2025-11-02 19:49:13 -0500

    allow metadata

--- dbs/vectordb/qdrant_client.py
+++ dbs/vectordb/qdrant_client.py
@@ -5,7 +5,8 @@ from qdrant_client import QdrantClient
 DB_PATH = "my_db"
 
 client = QdrantClient(path=DB_PATH)
-# Set the default embedding model
+
+# Optionally set the embedding model - default is BAAI/bge-small-en
 client.set_model("BAAI/bge-small-en")
 
 
@@ -14,13 +15,21 @@ def clean_collection(collection_name: str):
     client.delete_collection(collection_name)
     return "Collection cleaned successfully"
 
-def add_documents(collection_name: str, documents: list[str], ids: list[str] = None):
+
+def add_documents(
+    collection_name: str,
+    documents: list[str],
+    ids: list[str] = None,
+    metadata: list[dict] = None,
+):
     """Add a document to the collection. Collection will be auto-created if it doesn't exist."""
     if ids is None:
         ids = [str(uuid.uuid4()) for _ in documents]
+
     client.add(
         collection_name=collection_name,
         documents=documents,
+        metadata=metadata,
         ids=ids,
     )
     return "Document added successfully"
